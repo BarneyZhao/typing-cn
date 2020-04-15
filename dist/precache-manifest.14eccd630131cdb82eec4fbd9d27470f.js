2acdf5139e3938b21dfb7eb9b4af2e29cba9f77c
赵烜梓 <zhaoxuanzi@jd.com>
2020-04-15 15:41:01 +0800

    gitee page support

---
+++ dist/precache-manifest.14eccd630131cdb82eec4fbd9d27470f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e9c9d04e3f6f64914e70ff1fa631d2b",
+    "url": "/typing-cn/index.html"
+  },
+  {
+    "revision": "0d79600dcc9227e79a9f",
+    "url": "/typing-cn/static/css/2.4af70dfe.chunk.css"
+  },
+  {
+    "revision": "3ba287eb45907ad8c777",
+    "url": "/typing-cn/static/css/main.69d50489.chunk.css"
+  },
+  {
+    "revision": "0d79600dcc9227e79a9f",
+    "url": "/typing-cn/static/js/2.6dde72f0.chunk.js"
+  },
+  {
+    "revision": "815e4501ec0af78aca983eab952b06f6",
+    "url": "/typing-cn/static/js/2.6dde72f0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3ba287eb45907ad8c777",
+    "url": "/typing-cn/static/js/main.c17cb16f.chunk.js"
+  },
+  {
+    "revision": "cbc25df2d327e374e390",
+    "url": "/typing-cn/static/js/runtime-main.f53856fb.js"
+  }
+]);
