57834021f3ff74645b1cf9a2fa991072de856dc5
zhaoxuanzi <zhaoxuanzi@jd.com>
2021-12-09 21:52:34 +0800

    docs

---
+++ docs/precache-manifest.a7f5f02c68fb64dde5d96f5ddd241149.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b661bd3226ebf14773c22aea629dc7e8",
+    "url": "/typing-cn/index.html"
+  },
+  {
+    "revision": "b84c616f4d0d3668fe6f",
+    "url": "/typing-cn/static/css/2.a92a4962.chunk.css"
+  },
+  {
+    "revision": "b0caea423b621a557d28",
+    "url": "/typing-cn/static/css/4.4f0730c5.chunk.css"
+  },
+  {
+    "revision": "a3c3bf9394891d87b6b5",
+    "url": "/typing-cn/static/css/5.06026757.chunk.css"
+  },
+  {
+    "revision": "3456c7907e594b1ccc6d",
+    "url": "/typing-cn/static/css/6.d253b7f8.chunk.css"
+  },
+  {
+    "revision": "8370344e76eef5f21bc1",
+    "url": "/typing-cn/static/css/7.6f1107f5.chunk.css"
+  },
+  {
+    "revision": "917a5aa9cdf47beba15f",
+    "url": "/typing-cn/static/css/8.e29a173a.chunk.css"
+  },
+  {
+    "revision": "7b2b00dd974e62a354bd",
+    "url": "/typing-cn/static/css/9.b9dba844.chunk.css"
+  },
+  {
+    "revision": "04e161d32cfe0386f62b",
+    "url": "/typing-cn/static/css/main.5d11dddb.chunk.css"
+  },
+  {
+    "revision": "b63960a9eae2dd0bee7c",
+    "url": "/typing-cn/static/js/10.9da8780d.chunk.js"
+  },
+  {
+    "revision": "b84c616f4d0d3668fe6f",
+    "url": "/typing-cn/static/js/2.4645502c.chunk.js"
+  },
+  {
+    "revision": "72f2e664ab070842309691c6f16bfeee",
+    "url": "/typing-cn/static/js/2.4645502c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2de1653f66bfe11db995",
+    "url": "/typing-cn/static/js/3.4f121027.chunk.js"
+  },
+  {
+    "revision": "b0caea423b621a557d28",
+    "url": "/typing-cn/static/js/4.3b159f58.chunk.js"
+  },
+  {
+    "revision": "a3c3bf9394891d87b6b5",
+    "url": "/typing-cn/static/js/5.c06cbeba.chunk.js"
+  },
+  {
+    "revision": "3456c7907e594b1ccc6d",
+    "url": "/typing-cn/static/js/6.aab25421.chunk.js"
+  },
+  {
+    "revision": "8370344e76eef5f21bc1",
+    "url": "/typing-cn/static/js/7.1a32cb4f.chunk.js"
+  },
+  {
+    "revision": "917a5aa9cdf47beba15f",
+    "url": "/typing-cn/static/js/8.bb5172eb.chunk.js"
+  },
+  {
+    "revision": "7b2b00dd974e62a354bd",
+    "url": "/typing-cn/static/js/9.c757a642.chunk.js"
+  },
+  {
+    "revision": "04e161d32cfe0386f62b",
+    "url": "/typing-cn/static/js/main.c8a26bfc.chunk.js"
+  },
+  {
+    "revision": "5fe29ee119fdcc292fc1",
+    "url": "/typing-cn/static/js/runtime-main.43bfacab.js"
+  },
+  {
+    "revision": "523145c8f6578ba0c73b4efe9453ff3f",
+    "url": "/typing-cn/static/media/alipay.523145c8.jpg"
+  },
+  {
+    "revision": "023559334415583c3ef829abdbd19eb0",
+    "url": "/typing-cn/static/media/avatar.02355933.jpg"
+  },
+  {
+    "revision": "01fd8a9c158af41a3550e936483eee39",
+    "url": "/typing-cn/static/media/beep.01fd8a9c.mp3"
+  },
+  {
+    "revision": "bf0173a5db962d2d15314daea2d884bf",
+    "url": "/typing-cn/static/media/wechatpay.bf0173a5.jpg"
+  }
+]);
