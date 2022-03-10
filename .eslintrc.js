b4a300f166e5b3662f88e7821dc79926f5ee41f0
zhaoxuanzi <zhaoxuanzi@jd.com>
2022-03-10 18:41:52 +0800

    fix: site icon change fix

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    extends: 'react-app',
+    plugins: ['prettier'],
+    rules: {
+        'prettier/prettier': 'error',
+        'import/no-anonymous-default-export': 0,
+    },
+};
