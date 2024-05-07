1d50a942e38c691a6e9a6b5617cf077fc0471d58
BarneyZhao <zxzswa@163.com>
2024-05-07 17:19:25 +0800

    build: 支持nobase构建

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import legacy from '@vitejs/plugin-legacy';
-import { visualizer } from 'rollup-plugin-visualizer';
+// import { visualizer } from 'rollup-plugin-visualizer';
 
 import pkg from './package.json';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-    base: mode === 'production' ? '/typing-cn/' : '',
+    base: mode === 'production' ? '/typing-cn/' : '/',
     define: { __APP_VERSION__: JSON.stringify(pkg.version) },
     plugins: [
         react(),
         legacy({
             targets: ['defaults', 'not IE 11'],
         }),
-        visualizer({ open: true, brotliSize: true, filename: 'report.html' }),
+        // visualizer({ open: true, brotliSize: true, filename: 'report.html' }),
     ],
     resolve: { alias: { '@': '/src' } },
     build: { target: 'es2015' },
